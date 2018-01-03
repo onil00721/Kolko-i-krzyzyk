fade5c241bc979c75f88118d973387fc13037949
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-03 20:45:53 +0100

    1

---
+++ Coś/public_html/newjavascript.js
@@ -0,0 +1,11 @@
+
+
+  
+                
+    $( ".kratka " ).click(function() {
+        $( this ).css( {"opacity":"0.9", "transition": " 1s" });  
+  //$( this ).css( {"opacity":"1", "transition": " 1s" });
+ // $( this ).html( '<i class=" icon-circle-empty"></i>');//O
+  $( this ).html( '<i class="demo-icon icon-cancel" style="opacity: 1;"></i>');//X
+
+});
