cd815fa6eb6fc81fbcb610c4739c89f5af514da6
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-04 13:37:46 +0100

    04,01
    edit jq

--- Coś/public_html/newjavascript.js
+++ Coś/public_html/newjavascript.js
@@ -1,11 +1,19 @@
 
+var kolejka= false; // false= wstaw kółko, true wstaw krzyżyk
 
+$( ".kratka " ).click(function() {     
   
-                
-    $( ".kratka " ).click(function() {
-        $( this ).css( {"opacity":"0.9", "transition": " 1s" });  
-  //$( this ).css( {"opacity":"1", "transition": " 1s" });
- // $( this ).html( '<i class=" icon-circle-empty"></i>');//O
-  $( this ).html( '<i class="demo-icon icon-cancel" style="opacity: 1;"></i>');//X
-
+      if ( kolejka == false ){             
+        $( this ).html( '<i class=" icon-circle-empty" style="opacity: 1;"></i>'); // wstawia kółko O    
+         kolejka = true; 
+        }
+    
+    else{                
+        $( this ).html( '<i class="demo-icon icon-cancel" style="opacity: 1;"></i>');//X
+         kolejka = false; 
+         }
+    
 });
+                
+ 
+                      
