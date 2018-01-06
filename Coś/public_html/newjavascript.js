51c2a4c2043089939d3aff764c0ac8a2d10bf88e
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-06 18:39:49 +0100

    6,01,18,39
    edit js

--- Coś/public_html/newjavascript.js
+++ Coś/public_html/newjavascript.js
@@ -11,7 +11,7 @@ var siedem= false;
 var osiem= false;
 var dziewiec= false;
 
-var jeden_Wygrana;
+var jeden_Wygrana;     //wartość false odpowiada za wartość O a true  X
 var dwa_Wygrana;
 var trzy_Wygrana;
 var cztery_Wygrana;
@@ -21,9 +21,7 @@ var siedem_Wygrana;
 var osiem_Wygrana;
 var dziewiec_Wygrana ;
 
-var zwracamZnak;
-
-var nrKratki;  //Zwraca numer kratki do której się odwołujemy
+var nrKratki=0;  //Zwraca numer kratki do której się odwołujemy
 
 
 
@@ -33,10 +31,7 @@ function myFun(){  //funkcja wstawiająca odpowiedni znak do kratki
         if  (znak == false)  {             
             $(nrKratki).html( '<i class=" icon-circle-empty" style="opacity: 1;"></i>'); // wstawia kółko O               
              znak = true; 
-            jeden_Wygrana=false;
-            
-            console.log(jeden_Wygrana);
-            
+           
             }
 
         else{                
@@ -48,72 +43,140 @@ function myFun(){  //funkcja wstawiająca odpowiedni znak do kratki
     }
 
 
-function jakiZnak(){    //Wzraca wartość czy O czy X
-    
-    $( nrKratki).click(function() {  
-      var htmlString = $( this ).html();
-    
-    if ( (htmlString) ==  '<i class=" icon-circle-empty" style="opacity: 1;"></i>' ){     
-        console.log("O");
+function jakiZnak(){    //Wzraca wartość czy O czy X    
+           
+      var htmlString = $(nrKratki).html();    
+    if ( htmlString ==  '<i class=" icon-circle-empty" style="opacity: 1;"></i>' ){   
+       
+            if(nrKratki=="#1"){
+                    jeden_Wygrana=false;
+                }
         
-      
-        zwracamZnak=23;
-         console.log(zwracamZnak);
-    }  
-        else{
-            console.log("X");
+             else if (nrKratki=="#2"){
+                    dwa_Wygrana=false;
+                }
+             else if (nrKratki=="#3"){
+                    trzy_Wygrana=false;
+                }
+             else if (nrKratki=="#4"){
+                    cztery_Wygrana=false;
+                }
+             else if (nrKratki=="#5"){
+                    piec_Wygrana==false;
+                }
+             else if (nrKratki=="#6"){
+                    szesc_Wygrana=false;
+                }
+             else if (nrKratki=="#7"){
+                    sziedem_Wygrana=false;
+                }
+             else if (nrKratki=="#8"){
+                    osiem_Wygrana=false;
+                }
+             else if (nrKratki=="#9"){
+                    dziewiec_Wygrana=false;
+                } 
+                 
+    } 
+    
+     else{
+         
+            if(nrKratki=="#1"){
+                    jeden_Wygrana=true;
+                }
+        
+             else if (nrKratki==="#2"){
+                    dwa_Wygrana=true;
+                }
+             else if (nrKratki=="#3"){
+                    trzy_Wygrana=true;
+                }
+             else if (nrKratki=="#4"){
+                    cztery_Wygrana=true;
+                }
+             else if (nrKratki=="#5"){
+                    piec_Wygrana=true;
+                }
+             else if (nrKratki=="#6"){
+                    szesc_Wygrana=true;
+                }
+             else if (nrKratki=="#7"){
+                    sziedem_Wygrana=true;
+                }
+             else if (nrKratki=="#8"){
+                    osiem_Wygrana=true;
+                }
+             else if (nrKratki=="#9"){
+                    dziewiec_Wygrana=true;
+                }
+
+         
+         
         }
     
-        });
-        }
+ }
+
+
+function ktoWygral(){
     
+    if( jeden_Wygrana && dwa_Wygrana && trzy_Wygrana == true){
+        console.log("Wygrał X 123");
+    }
+    
+    else if(){
+             console.log("Ktoś chyba wygrał");
+            }
+    
+    
+    
+}
 
-
-
-
-
-
-
-
-
-
+    
 
 $( "#1" ).click(function() {     //akcja dla kratki 1
-nrKratki='#1';
-   
-    
+      nrKratki="#1";   
+       
     if ( jeden==false){    
         myFun();          //wstawiamy O albo X
-    }  
-     jeden=true;
-    jakiZnak();     //Zwraca informację o wstawionym znaku do kratki 
+    } 
     
-    console.log(nrKratki);
+     jeden=true;    //blokuje możliwość ponownego wstawienia znaku 
+     jakiZnak();     //Zwraca informację o wstawionym znaku do kratki w postaci true albo false   
+     ktoWygral();   
+
     
-    
-     
 });
 
 
-
-$( "#2" ).click(function() {     
-nrKratki='#2';
-    
+$( "#2" ).click(function() {     //akcja dla kratki 1
+      nrKratki="#2";   
+       
     if ( dwa==false){    
-        myFun();          
-    }   
-    dwa=true;
+        myFun();          //wstawiamy O albo X
+    } 
+    
+     dwa=true;    //blokuje możliwość ponownego wstawienia znaku 
+     jakiZnak();     //Zwraca informację o wstawionym znaku do kratki w postaci true albo false   
+     ktoWygral();   
+
+    
 });
 
 
-
-$( "#3" ).click(function() {     
-nrKratki='#3';
-    
+$( "#3" ).click(function() {     //akcja dla kratki 1
+      nrKratki="#3";   
+       
     if ( trzy==false){    
-        myFun();          
-    }   
-    trzy=true;
+        myFun();          //wstawiamy O albo X
+    } 
+    
+     trzy=true;    //blokuje możliwość ponownego wstawienia znaku 
+     jakiZnak();     //Zwraca informację o wstawionym znaku do kratki w postaci true albo false   
+     ktoWygral();   
+console.log(jeden_Wygrana);
+console.log(dwa_Wygrana);
+console.log(trzy_Wygrana);
+    
 });
 
 
