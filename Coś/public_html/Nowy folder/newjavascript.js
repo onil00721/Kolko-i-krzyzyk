2b1601910cbe3fbe4394ebca3820e3899298034a
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-10 08:55:18 +0100

    Revert "Revert "Revert "przymusuowa edycja"""
    This reverts commit bc7d22c13510481a4883f36ff0596b697ef7bace.

--- Coś/public_html/Nowy folder/newjavascript.js
+++ Coś/public_html/Nowy folder/newjavascript.js
@@ -1,13 +1,31 @@
    
 
 
-$(document).ready(function(){
-    $("button").click(function(){
-         $("#div1").fadeTo("slow", 0.15);
-        $(".kratka").fadeTo("slow", 0.3);
+
+
+
+
+
+
+var a=false;
+var b=false;
+var c=false;
+           // var c;
+            document.write(a+"    ");    
+            document.write(b+"    ");    
+           document.write(c+"    ");    
+           
+            if( a&&b&&c== false){
+                    document.write("a + b =t  ");    
+            
+            
+            
+            } 
+            
+             else{
+                document.write("nnnniiiicicii");
+
+             }
+            
         
-    });
-});
-
-
         
