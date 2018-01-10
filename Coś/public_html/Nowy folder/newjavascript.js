9f46912c96b861242148d5cb0a9f306ce8243573
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-10 08:38:15 +0100

    przymusuowa edycja

--- Coś/public_html/Nowy folder/newjavascript.js
+++ Coś/public_html/Nowy folder/newjavascript.js
@@ -1,31 +1,13 @@
    
 
 
-
-
-
-
-
-
-var a=false;
-var b=false;
-var c=false;
-           // var c;
-            document.write(a+"    ");    
-            document.write(b+"    ");    
-           document.write(c+"    ");    
-           
-            if( a&&b&&c== false){
-                    document.write("a + b =t  ");    
-            
-            
-            
-            } 
-            
-             else{
-                document.write("nnnniiiicicii");
-
-             }
-            
+$(document).ready(function(){
+    $("button").click(function(){
+         $("#div1").fadeTo("slow", 0.15);
+        $(".kratka").fadeTo("slow", 0.3);
         
+    });
+});
+
+
         
