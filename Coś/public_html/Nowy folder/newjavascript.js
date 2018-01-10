760a4f016aa2bbfe908dc745967ba493db6eef11
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-10 20:19:24 +0100

    20,19

--- Coś/public_html/Nowy folder/newjavascript.js
+++ Coś/public_html/Nowy folder/newjavascript.js
@@ -1,23 +1,52 @@
    
  
 var tab=[];
-
-  tab[1]=18 
+var tab2=[];
+var tab3=[];
+ var z=0;
   tab[0]=18
+  tab[1]=18   
   tab[2]=15  
-  tab[3]=12  
+  tab[3]=12
   tab[4]=12  
   tab[5]=14  
-tab.sort();
-console.log(tab);
+//tab.sort();
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+
+
 
 
     for (x=0; x<tab.length; x++){
         
+       //  if 
         
+          
+          
+//if(tab[x]==tab2[x]){
+  //  console.log("równe");
+//}
+   tab2[x]=tab[x];
         
+         for (y=0; y<7; y++){
+             
+             
+             
+             if (tab[y]==tab2[x]){
+                 
+                 tab3[x]=tab[x];
+                //z++;
          
-     console.log(x);
+    /* console.log(tab3[0]+"ggggggggg");
+     console.log(tab3[1]);
+     console.log(tab3[2]);
+     console.log(tab3[3]);
+     console.log(tab3[4]);
+     console.log(tab3[5]);
+     console.log(tab3[6]);
+      */       }
+    
      //console.log(tab[x]);
         
     }
@@ -27,9 +56,16 @@ console.log(tab);
     
         
     
+    }
+ for (var a=0; a<tab3.length; a++){
+   console.log(tab3[a]);
+     
+ }
+     
+console.log(tab3);
 
-
-
-
+//var cos= tab3.lengthh;
+//console.log(tab2.length);
+     
         
       
