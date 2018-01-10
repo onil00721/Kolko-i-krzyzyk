0585b44b21fdf010646afa50ab2a72fe8b271c75
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-10 19:08:48 +0100

    19,08

--- Coś/public_html/Nowy folder/newjavascript.js
+++ Coś/public_html/Nowy folder/newjavascript.js
@@ -1,27 +1,35 @@
    
-  var tab=[];
+ 
+var tab=[];
 
-  tab[1]=9  
-  tab[0]=10 
-  tab[2]=11  
+  tab[1]=18 
+  tab[0]=18
+  tab[2]=15  
   tab[3]=12  
-  tab[4]=13  
+  tab[4]=12  
   tab[5]=14  
+tab.sort();
+console.log(tab);
 
 
+    for (x=0; x<tab.length; x++){
+        
+        
+        
+         
+     console.log(x);
+     //console.log(tab[x]);
+        
+    }
+       
+ 
+    // console.log("X<2");
+    
+        
     
 
-    var j;
-      
-      
-        
 
 
-        console.log(tab[1]);
-        
-        var n = tab.length;
-        
-        console.log(n+"Wielkość tablicy ");
-        
+
         
       
