2fff94957b11791493d2b6ecce015bfee742f6dd
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-10 16:37:00 +0100

    10,01,16,36

--- Coś/public_html/Nowy folder/newjavascript.js
+++ Coś/public_html/Nowy folder/newjavascript.js
@@ -1,31 +1,27 @@
    
+  var tab=[];
+
+  tab[1]=9  
+  tab[0]=10 
+  tab[2]=11  
+  tab[3]=12  
+  tab[4]=13  
+  tab[5]=14  
 
 
+    
+
+    var j;
+      
+      
+        
 
 
-
-
-
-
-var a=false;
-var b=false;
-var c=false;
-           // var c;
-            document.write(a+"    ");    
-            document.write(b+"    ");    
-           document.write(c+"    ");    
-           
-            if( a&&b&&c== false){
-                    document.write("a + b =t  ");    
-            
-            
-            
-            } 
-            
-             else{
-                document.write("nnnniiiicicii");
-
-             }
-            
+        console.log(tab[1]);
+        
+        var n = tab.length;
+        
+        console.log(n+"Wielkość tablicy ");
         
         
+      
